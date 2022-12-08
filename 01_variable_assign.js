e9458c53870fb62d00a4766cdbd8bfa9c46199cb
yogeshmaske <yogeshmaske99@gmail.com>
2022-12-08 16:24:43 +0530

    assigment 1 created
    assigment 1 created

---
+++ 01_variable_assign.js
@@ -0,0 +1,21 @@
+var myFullName;
+myFullName = "Yogesh maske";
+console.log(myFullName);
+
+myFullName = " Yogesh Shriram Maske";
+console.log(myFullName);
+
+var myLove = " I Love Only JS";
+
+console.log(myLove);
+
+
+var firstName = "Sachin";
+var lastName = "Tendulkar";
+var age = 50;
+var address = "Jalna";
+var pin_code = 431203;
+var gender = "Male";
+var is_married = "Yes";
+
+console.log(firstName,lastName,age,address,pin_code,gender,is_married);
