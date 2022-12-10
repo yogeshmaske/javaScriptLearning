a15366e8ef6860d4cfb341e385a76f468fb30ebf
yogeshmaske <yogeshmaske99@gmail.com>
2022-12-10 11:12:36 +0530

    modified
    modified

---
+++ 01_typeOf.js
@@ -0,0 +1,24 @@
+var bankName;
+console.log(bankName);
+console.log(typeof bankName);
+bankName = "SBI Bank";
+console.log(bankName);
+console.log(typeof bankName);
+
+var marks ="90%" ;
+console.log(typeof marks);
+console.log(marks);
+
+var isWorking;
+console.log(isWorking);
+isWorking = true;
+// console.log(typeof isWorking);
+console.log(isWorking);
+console.log(typeof isWorking);
+
+var totalCount = "Hundred and Seven";
+console.log("Type of  ",totalCount," is  ",typeof totalCount );
+
+totalCount = 107;
+console.log(" After updating totalCount Variable..")
+console.log("Type of  ",totalCount," is  ",typeof totalCount );
