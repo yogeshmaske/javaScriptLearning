a443073472c28f62e68f33df53341e45cd825947
yogeshmaske <yogeshmaske99@gmail.com>
2022-12-21 14:56:02 +0530

    while and do while loop
    while and do while loop

---
+++ assigment_21.12.22.js
@@ -0,0 +1,68 @@
+
+// let index = 5;
+// while (index <= 15) {
+ 
+//     console.log(index)
+//     index++
+// }
+
+
+// let index = 50;
+// while (index >= 40) {
+    
+//     console.log(index)
+
+    
+//     index--;
+// }
+
+
+// odd numbers up 15
+// let index = 1;
+
+// while (index <= 15) {
+     
+//     if (index%2 !== 0) {
+//                 console.log(index)
+//                }
+
+//     index++
+// }
+
+// let index = 1
+// while (index <= 10) {
+//     if (index%2 == 0) {
+//              console.log(index)
+//             }
+        
+
+//     index++
+// }
+
+// let index =1 , j=5;
+// do {
+//     console.log(j*index);
+//     index++
+// } while (index <=10);
+
+
+// let index =1 , j=10;
+// do {
+//     console.log(j*index);
+//     index++
+// } while (index <=10);
+
+let index =10 , j=10;
+do {
+    console.log(j*index);
+    index--
+} while (index >=1);
+
+
+// for (let index = 10,j=10; index >= 1; index--){
+     
+//     console.log(j*index);
+
+// };
+
+
